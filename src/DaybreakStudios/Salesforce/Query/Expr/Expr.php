dd0d32df578609cdcde39643a98b6256c2decc7a
Tyler Lartonoix <larttyler@live.com>
2015-07-02 14:11:34 -0400

    I was wrong, apparently you can't create an object in the execute portion of a for loop

--- src/DaybreakStudios/Salesforce/Query/Expr/Expr.php
+++ src/DaybreakStudios/Salesforce/Query/Expr/Expr.php
@@ -8,32 +8,38 @@
 			$wheres = func_get_args();
 			$where = null;
 
-			for ($i = 0, $ii = sizeof($wheres); $i < $ii; $part = new AndPart($wheres[$i++]))
+			for ($i = 0, $ii = sizeof($wheres); $i < $ii; $i++) {
+				$part = new AndPart($wheres[$i]);
+
 				if ($where === null)
 					$where = $part->getValue();
 				else
 					$where .= $part->assemble();
+			}
 
 			if ($where === null)
 				throw new InvalidArgumentException('Expr#andX requires at least one argument');
 
-			return sprintf(' (%s) ', $where);
+			return sprintf('(%s)', $where);
 		}
 
 		public function orX(/* ... $wheres */) {
 			$wheres = func_get_args();
 			$where = null;
 
-			for ($i = 0, $ii = sizeof($wheres); $i < $ii; $part = new OrPart($wheres[$i++]))
+			for ($i = 0, $ii = sizeof($wheres); $i < $ii; $i++) {
+				$part = new OrPart($wheres[$i]);
+
 				if ($where === null)
 					$where = $part->getValue();
 				else
 					$where .= $part->assemble();
+			}
 
 			if ($where === null)
 				throw new InvalidArgumentException('Expr#orX requires at least one argument');
 
-			return sprintf(' (%s) ', $where);
+			return sprintf('(%s)', $where);
 		}
 	}
 ?>
